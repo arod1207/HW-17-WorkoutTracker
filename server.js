714e8ac881104573c20a07eac137298a38c94940
Armando Rodriguez <arod1207@me.com>
2020-08-12 14:03:36 -0500

    mongoose database connected

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const mongoose = require("./seeders/seed");
+const mongoose = require("mongoose");
 const logger = require("morgan");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => console.log("Connected to Database"));
 
 app.use(logger("dev"));
 
