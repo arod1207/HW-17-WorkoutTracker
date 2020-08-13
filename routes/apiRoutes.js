9d3ef3b9d1af89644bf617e35d3bbbcaa78220e3
Armando Rodriguez <arod1207@me.com>
2020-08-13 17:00:41 -0500

    working on a findbyId

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,8 +4,12 @@ const db = require("../models");
 
 Router.get("/api/workouts", (req, res) => {
   db.Workout.find({}).then((workout) => {
-    res.json(workout);
+    res.send(workout);
   });
+});
+
+Router.get("/api/workouts/:id", (req, res) => {
+  db.Workout.findById({ __id });
 });
 
 module.exports = Router;
