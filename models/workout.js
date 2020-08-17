37e964ccf1394193421c29f7ebe74de91a8489b8
Armando Rodriguez <arod1207@me.com>
2020-08-17 14:46:11 -0500

    testing heroku

--- models/workout.js
+++ models/workout.js
@@ -2,35 +2,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  day: { type: Date, default: Date.now },
+  day: {
+    type: Date,
+    default: Date.now,
+  },
   exercises: [
     {
       type: {
         type: String,
         trim: true,
-        required: "Enter type of exercise...",
       },
       name: {
         type: String,
         trim: true,
-        required: "Enter name of exercise ...",
       },
-      duration: {
-        type: Number,
-        required: "Enter duration of exercise...",
-        default: 1,
-      },
+      duration: Number,
       weight: {
         type: Number,
-        required: "Enter amount of weight...",
+        default: 0,
       },
       reps: {
         type: Number,
-        required: "Enter number of reps...",
+        default: 0,
       },
       sets: {
         type: Number,
-        required: "Enter number of sets...",
+        default: 0,
       },
       distance: {
         type: Number,
