418d32558edbfe7bb51f951d7e8825065ad49fee
Armando Rodriguez <arod1207@me.com>
2020-08-17 18:03:51 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(logger("dev"));
 
 const PORT = process.env.PORT || 3000;
 
-const MONGODB_URI =
-  process.env.MONGODB_URL ||
-  "mongodb://admin:@Password1@ds035702.mlab.com:35702/heroku_w1tg78h0";
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
@@ -18,15 +20,9 @@ mongoose.connect(MONGODB_URI, {
   useFindAndModify: false,
 });
 
-const db = mongoose.connection;
-
-db.once("open", () => console.log("Connected to Database"));
-
-app.use(logger("dev"));
-
-app.use(express.static("public"));
-
-require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
 
-app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, function () {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
