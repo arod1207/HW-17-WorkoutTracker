619bef5db0adaf9e9f66caf566ab55e959c84103
Armando Rodriguez <arod1207@me.com>
2020-08-17 23:20:53 -0500

    final push with hidden api key

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb://heroku_1zp8fxcd:nt7vsjepohhgtf1g7gu0gff1p1@ds249787.mlab.com:49787/heroku_1zp8fxcd";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
